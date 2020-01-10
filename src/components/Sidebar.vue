fd0a649a8d893b48e16da5ebe9e2ea7d721878ef
Stephen F <snipeydev@gmail.com>
2020-01-10 00:29:12 -0800

    Add title to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,7 +12,8 @@
 		<!-- Profile Picture -->
 		<div class="flex-0 p-4 text-center pt-2">
 			<img src="https://i.picsum.photos/id/893/200/200.jpg" class="rounded-full mx-auto mb-4 border-4 w-4/5 hover:border-purple-600 border-purple-400" />
-			<span class="text-white text-2xl font-semibold font-sans hover:underline pt-2">Stephen Freerking</span>
+			<h2 class="text-white text-2xl font-semibold font-sans hover:underline pt-2">Stephen Freerking</h2>
+			<pre class="text-white text-xl">Full Stack Developer</pre>
 			<Socials />
 		</div>
 	</div>
